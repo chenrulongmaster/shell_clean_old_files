24d129511c08977d1e60a48f2ec81971181dd420
GeekerSquare <chenrulong0513.master@gmail.com>
2020-06-23 14:53:47 +0800

    Update clean_files.sh

--- clean_files.sh
+++ clean_files.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 keep_days=5
 log_path=$1
@@ -9,10 +9,13 @@ file_num=$(find $log_path -type f -mtime +$keep_days | wc -l )
 
 for file in $file_name
 do
+    if [[ $file =~ ".gitkeep" ]]; then
+        continue
+    fi
     rm -f $file
     if [ $? -eq 0 ]; then
-        echo "$date_time 清理过期文件$file 成功" >> /tmp/$0.log
+        echo "$date_time 清理过期文件$file 成功" > /tmp/$0.log
     else
-        echo "$date_time 清理过期文件$file 失败" >> /tmp/$0.log
+        echo "$date_time 清理过期文件$file 失败" > /tmp/$0.log
     fi
 done
